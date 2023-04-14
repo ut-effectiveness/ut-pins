18d478db21b7d6fb925253d1f621fb041b4aec0b
Russ Hyde <russ@jumpingrivers.com>
2023-04-14 12:33:55 +0100

    test: get_pins_board fails with incorrect API key

--- tests/testthat/test-board.R
+++ tests/testthat/test-board.R
@@ -1,8 +1,17 @@
 describe(
   "get_pins_board",
-  it("returns a local pins board when under `test` setting", {
-    pb <- get_pins_board(setting = "test")
+  {
+    it("returns a local pins board when under `test` setting", {
+      pb <- get_pins_board(setting = "test")
 
-    expect_is(pb, "pins_board_folder")
-  })
+      expect_is(pb, "pins_board_folder")
+    })
+    it("errors with an incorrect API key", {
+      withr::local_envvar("***REMOVED***" = "NOT-AN-API-KEY")
+
+      expect_error(
+        get_pins_board(setting = "prod")
+      )
+    })
+  }
 )
