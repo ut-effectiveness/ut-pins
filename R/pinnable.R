1b5a8f393e20b8f0594ca3bfdee79a497c5a5eca
Russ Hyde <russ@jumpingrivers.com>
2023-04-14 15:26:06 +0100

    feat: add method to prepare dataset for a pinnable object

--- R/pinnable.R
+++ R/pinnable.R
@@ -1,6 +1,26 @@
-pinnable <- function() {
+pinnable <- function(prepare_fn = NULL) {
   structure(
-    list(),
+    list(
+      prepare_fn = prepare_fn
+    ),
     class = "pinnable"
   )
+}
+
+#' Prepare a dataset for publication to pins
+#'
+#' @export
+prepare <- function(x) {
+  UseMethod("prepare")
+}
+
+#' @export
+prepare.default <- function(x) {
+  stop("Default prepare method is unimplemented")
+}
+
+#' @export
+prepare.pinnable <- function(x) {
+  x$data <- x$prepare_fn()
+  x
 }
--- tests/testthat/test-pinnable.R
+++ tests/testthat/test-pinnable.R
@@ -5,3 +5,19 @@ describe("pinnable()", {
     expect_is(p, "pinnable")
   })
 })
+
+describe("prepare(pinnable)", {
+  it("creates a new `pinnable` with a `data` entry", {
+    p <- pinnable(
+      prepare = function() datasets::iris
+    )
+
+    expect_true(
+      "data" %in% names(prepare(p))
+    )
+    expect_equal(
+      prepare(p)$data,
+      datasets::iris
+    )
+  })
+})
