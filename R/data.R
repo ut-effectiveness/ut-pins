380ed91ea3ce72967fdf8eb40f662242eb01972e
Russ Hyde <russ@jumpingrivers.com>
2023-04-24 13:11:49 +0100

    data: add a dataset containing contracted workloads

--- R/data.R
+++ R/data.R
@@ -1,2 +1,8 @@
 #' apr_supplemental_data
 "apr_supplemental_data"
+
+#' contracted_workload
+#'
+#' Data frame with three columns `sis_id`, `term`, `contracted_workload`.
+#' The `sis_id` values are 8-digit identifiers with leading zeroes.
+"contracted_workload"
