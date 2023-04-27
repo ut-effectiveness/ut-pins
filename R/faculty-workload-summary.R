273b901c6fff075568817a43cc881b64b078836d
Russ Hyde <russ@jumpingrivers.com>
2023-04-27 16:10:51 +0100

    fix: namespace all external functions (in get_summarized_faculty_workload_df)

--- R/faculty-workload-summary.R
+++ R/faculty-workload-summary.R
@@ -18,11 +18,14 @@ get_summarized_faculty_workload_df <- function(instructional_faculty_workload_df
       instructional_workload = sum(course_workload),
       # For courses_taught we count distinct course_crn if the course is NOT a cross listed course,
       # otherwise we count all cross listed course sections as one single course.
-      courses_taught = n_distinct(course_crn[is.na(course_cross_list_group)]) + n_distinct(course_cross_list_group[!is.na(course_cross_list_group)]),
-      faculty_status = first(na.omit(faculty_status)),
-      faculty_rank = first(na.omit(faculty_rank)),
-      faculty_college = first(na.omit(faculty_college)),
-      faculty_department = first(na.omit(faculty_department)),
+      courses_taught = (
+        dplyr::n_distinct(course_crn[is.na(course_cross_list_group)]) +
+          dplyr::n_distinct(course_cross_list_group[!is.na(course_cross_list_group)])
+      ),
+      faculty_status = dplyr::first(na.omit(faculty_status)),
+      faculty_rank = dplyr::first(na.omit(faculty_rank)),
+      faculty_college = dplyr::first(na.omit(faculty_college)),
+      faculty_department = dplyr::first(na.omit(faculty_department)),
       contracted_workload = min(contracted_workload)
     ) %>%
     dplyr::ungroup()
@@ -33,10 +36,10 @@ get_summarized_faculty_workload_df <- function(instructional_faculty_workload_df
     dplyr::group_by_at(grouping) %>%
     dplyr::summarize(
       non_instructional_workload = sum(non_instructional_workload),
-      faculty_status = first(na.omit(faculty_status)),
-      faculty_rank = first(na.omit(faculty_rank)),
-      faculty_college = first(na.omit(faculty_college)),
-      faculty_department = first(na.omit(faculty_department)),
+      faculty_status = dplyr::first(na.omit(faculty_status)),
+      faculty_rank = dplyr::first(na.omit(faculty_rank)),
+      faculty_college = dplyr::first(na.omit(faculty_college)),
+      faculty_department = dplyr::first(na.omit(faculty_department)),
       contracted_workload = min(contracted_workload)
     ) %>%
     dplyr::ungroup()
@@ -47,35 +50,35 @@ get_summarized_faculty_workload_df <- function(instructional_faculty_workload_df
     y = summarized_non_instructional_faculty_workload_df,
     by = grouping
   ) %>%
-    mutate(
-      faculty_status = coalesce(faculty_status.x, faculty_status.y),
-      faculty_rank = coalesce(faculty_rank.x, faculty_rank.y),
-      faculty_college = coalesce(faculty_college.x, faculty_college.y),
-      faculty_department = coalesce(faculty_department.x, faculty_department.y),
-      contracted_workload = coalesce(contracted_workload.x, contracted_workload.y)
+    dplyr::mutate(
+      faculty_status = dplyr::coalesce(faculty_status.x, faculty_status.y),
+      faculty_rank = dplyr::coalesce(faculty_rank.x, faculty_rank.y),
+      faculty_college = dplyr::coalesce(faculty_college.x, faculty_college.y),
+      faculty_department = dplyr::coalesce(faculty_department.x, faculty_department.y),
+      contracted_workload = dplyr::coalesce(contracted_workload.x, contracted_workload.y)
     ) %>%
-    select(-c(
+    dplyr::select(-c(
       faculty_status.x, faculty_status.y,
       faculty_rank.x, faculty_rank.y,
       faculty_college.x, faculty_college.y,
       faculty_department.x, faculty_department.y,
       contracted_workload.x, contracted_workload.y
     )) %>%
-    mutate(
-      instructional_workload = replace_na(instructional_workload, 0),
-      non_instructional_workload = replace_na(non_instructional_workload, 0),
-      contracted_workload = replace_na(contracted_workload, 0)
+    dplyr::mutate(
+      instructional_workload = tidyr::replace_na(instructional_workload, 0),
+      non_instructional_workload = tidyr::replace_na(non_instructional_workload, 0),
+      contracted_workload = tidyr::replace_na(contracted_workload, 0)
     ) %>%
     # Final workload and overload calculations. ####
-    mutate(
+    dplyr::mutate(
       total_workload = instructional_workload + non_instructional_workload,
-      overload = case_when(
+      overload = dplyr::case_when(
         contracted_workload == 0 ~ 0,
         TRUE ~ total_workload - contracted_workload
       )
     ) %>%
     # set the order of columns
-    relocate(c(
+    dplyr::relocate(c(
       "faculty_info", "sis_id", "full_name", time_aggregator,
       "faculty_college", "faculty_department", "faculty_status", "faculty_rank",
       "instructional_workload", "non_instructional_workload", "total_workload",
