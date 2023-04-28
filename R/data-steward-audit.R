6545937cc9c8880eee44e2e9887180d09504ae8a
Russ Hyde <russ@jumpingrivers.com>
2023-04-28 15:20:42 +0100

    feat: function to get data-steward-audit datasets

---
+++ R/data-steward-audit.R
@@ -0,0 +1,24 @@
+#' Get a data-frame containing a data-steward-audit dataset
+#'
+#' @param   audit_type   Scalar character. The type of audit report to generate. Allowed values:
+#'   `student`, `student_course`, `room`, `graduation`, `course`, `building`.
+#' @param   dsn   A DSN entry: `***REMOVED***`, `***REMOVED***`, `***REMOVED***`, `***REMOVED***`, etc...
+#'
+#' @export
+
+get_data_steward_audit <- function(audit_type = c(
+                                     "student",
+                                     "student_course",
+                                     "room",
+                                     "graduation",
+                                     "course",
+                                     "building",
+                                   ),
+                                   dsn = "***REMOVED***") {
+  audit_type <- match.arg(audit_type)
+
+  sql_file <- system.file(
+    "sql", "data_steward_audit", paste0(audit_type, ".sql", package = "utPins", mustWork = TRUE)
+  )
+  utHelpR::get_data_from_sql_file(sql_file, dsn = dsn)
+}
