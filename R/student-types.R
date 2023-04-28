29253eafd42d320100d9d8538785f95c753fd96a
Russ Hyde <russ@jumpingrivers.com>
2023-04-28 09:40:58 +0100

    copy calculate_student_types from scheduled-pins

---
+++ R/student-types.R
@@ -0,0 +1,96 @@
+calculate_student_types <- function(student_type_determination_variables,
+                                    parameter_term,
+                                    term_two_terms_ago) {
+  return_df <- student_type_determination_variables %>%
+    mutate(calculated_student_type = case_when(
+      # GROUP: High School ####
+      # if the calculated high school graduation term is greater than the passed in parameter term,
+      # then the student is a high school student on the provided parameter term
+      # if calculated_high_school_graduation_term > parameter_term
+      #    return "High School"
+      calculated_high_school_graduation_term > parameter_term
+      ~ "High School",
+
+      # GROUP Graduate ####
+      # first_term_enrolled and last_term_enrolled only looking at GRADUATE LEVEL transcript
+      # records for local institution
+
+      ## Continuing Graduate ####
+      # if last_term_enrolled_as_graduate >= (parameter_term - two_terms)
+      #    return "Continuing Graduate"
+      last_term_enrolled_on_or_after_calculated_hs_graduation_term >= term_two_terms_ago &
+        student_level == "GR"
+      ~ "Continuing Graduate",
+
+      ## Readmit Graduate ####
+      # if last_term_enrolled_as_graduate < (parameter_term - two_terms)
+      #    return "Readmit Graduate"
+      last_term_enrolled_on_or_after_calculated_hs_graduation_term < term_two_terms_ago &
+        student_level == "GR"
+      ~ "Readmit Graduate",
+
+      ## Transfer Graduate ####
+      # if has_transfer_graduate_credits & ( first_term_enrolled_as_graduate == parameter_term )
+      #    return "Transfer Graduate"
+      has_transfer_credits_on_or_after_calculated_hs_graduation_term &
+        first_term_enrolled_on_or_after_calculated_hs_graduation_term == parameter_term &
+        student_level == "GR" &
+        transfer_credits_level == "GR"
+      ~ "Transfer Graduate",
+
+      ## New Graduate ####
+      # if first_term_enrolled_as_graduate == parameter_term:
+      #    return "New Graduate"
+      first_term_enrolled_on_or_after_calculated_hs_graduation_term == parameter_term &
+        student_level == "GR"
+      ~ "New Graduate",
+
+      # GROUP Undergraduate ####
+      # first_term_enrolled, last_term_enrolled after calculated_high_school_graduation_date
+      # and only looking at UNDERGRADUATE LEVEL transcript records for local institution.
+
+      ## Continuing ####
+      # if last_term_enrolled_as_undergraduate_after_hs_grad >= (parameter_term - two_terms):
+      #  return "Continuing Undergraduate"
+      last_term_enrolled_on_or_after_calculated_hs_graduation_term >= term_two_terms_ago &
+        student_level == "UG"
+      ~ "Continuing Undergraduate",
+
+      ## Readmit ####
+      # if last_term_enrolled_as_undergraduate_after_hs_grad < (parameter_term - two_terms):
+      #  return "Readmit Undergraduate"
+      last_term_enrolled_on_or_after_calculated_hs_graduation_term < term_two_terms_ago &
+        student_level == "UG"
+      ~ "Readmit Undergraduate",
+
+      ## Transfer ####
+      # has_transfer_undergraduate_credits after calculated_high_school_graduation_date.
+      # if has_transfer_undergraduate_credits_after_hs_grad
+      #   AND (first_term_enrolled_as_undergraduate_after_hs_grad == parameter_term):
+      #  return "Transfer Undergraduate"
+      has_transfer_credits_on_or_after_calculated_hs_graduation_term &
+        first_term_enrolled_on_or_after_calculated_hs_graduation_term == parameter_term &
+        student_level == "UG" &
+        transfer_credits_level == "UG"
+      ~ "Transfer Undergraduate",
+
+      ## Freshman ####
+      # if first_term_enrolled_as_undergraduate_after_hs_grad == parameter_term:
+      #  return "Freshman"
+      first_term_enrolled_on_or_after_calculated_hs_graduation_term == parameter_term &
+        student_level == "UG"
+      ~ "Freshman",
+
+      ## Unknown ####
+      # else:
+      # return "Unknown"
+      TRUE
+      ~ "Unknown"
+    ), ) %>%
+    # set term for what is used in calculations
+    mutate(term_id = parameter_term) %>%
+    # only return relevant variables from calculation
+    select(sis_system_id, term_id, calculated_student_type)
+
+  return(return_df)
+}
