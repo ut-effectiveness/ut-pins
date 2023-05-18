636b48ab72d31e6630d601fdccbf6a13675366f1
Russ Hyde <russ@jumpingrivers.com>
2023-05-18 15:34:21 +0100

    fix: ensure audit-report and data-steward sql filepaths are correct

--- R/data-steward-audit.R
+++ R/data-steward-audit.R
@@ -2,7 +2,7 @@
 #'
 #' @param   audit_type   Scalar character. The type of audit report to generate. Allowed values:
 #'   `student`, `student_course`, `room`, `graduation`, `course`, `building`.
-#' @param   dsn   A DSN entry: `***REMOVED***`, `***REMOVED***`, `***REMOVED***`, `***REMOVED***`, etc...
+#' @inheritParams   get_data_from_sql_query
 #'
 #' @export
 
@@ -18,7 +18,10 @@ get_data_steward_audit <- function(audit_type = c(
   audit_type <- match.arg(audit_type)
 
   sql_file <- system.file(
-    "sql", "data_steward_audit", paste0(audit_type, ".sql"), package = "utPins", mustWork = TRUE
+    "sql", "data_steward_audit", paste0(audit_type, ".sql"),
+    package = "utPins", mustWork = TRUE
   )
-  utHelpR::get_data_from_sql_file(sql_file, dsn = dsn)
+  sql_query <- readr::read_file(sql_file)
+
+  get_data_from_sql_query(sql_query, dsn = dsn)
 }
