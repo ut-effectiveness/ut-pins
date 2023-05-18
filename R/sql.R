b4e23c78cb172b5437bb44e9bd0d5e149a6edafa
Russ Hyde <russ@jumpingrivers.com>
2023-05-18 14:59:52 +0100

    feat: functions to get SQL query string and perform SQL query

---
+++ R/sql.R
@@ -0,0 +1,39 @@
+#' Query a Utah Tech database with a SQL string
+#'
+#' @param   query_string   Scalar character. An SQL query string for use when querying a database.
+#' @param   dsn   A DSN entry: `***REMOVED***`, `***REMOVED***`, `***REMOVED***`, `***REMOVED***`, etc...
+#'
+#' @return   data.frame. The cleaned-up database entries that match the query string.
+#' @export
+
+get_data_from_sql_query <- function(query_string, dsn) {
+  conn <- utHelpR::get_connection_object(dsn)
+
+  df <- DBI::dbGetQuery(conn, query_string) %>%
+    utHelpR::mung_dataframe()
+
+  return(df)
+}
+
+#' Obtain a SQL query string from one of the files in `./inst/sql/`
+#'
+#' This defines the query that should be made against the UTAH databases for a given query_type
+#'
+#' @param   query_type   Scalar character. Valid values are `daily_enrollment`. Defines the type of
+#' query that is to be made. Invalid values will evoke an error.
+#'
+#' @return   SQL query string for use in `DBI::dbGetQuery`.
+#' @export
+
+get_query_string <- function(query_type) {
+  sql_files <- list(
+    daily_enrollment = system.file(
+      "sql", "admissions", "daily_enrollment.sql",
+      package = "utPins", mustWork = TRUE
+    )
+  )
+
+  stopifnot(query_type %in% names(sql_files))
+
+  readr::read_file(sql_files[[query_type]])
+}
